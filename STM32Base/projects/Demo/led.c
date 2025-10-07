1f94a0c0904dd06238061a555e9afdaeadd327b3
flashfishgit <102473074+flashfishgit@users.noreply.github.com>
2025-10-07 15:26:44 +0200

    erste uebung

--- STM32Base/projects/Demo/led.c
+++ STM32Base/projects/Demo/led.c
@@ -15,6 +15,10 @@
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
+	#define LED3 GPIO_ODR_0
+	#define LED4 GPIO_ODR_1
+	#define LED5 GPIO_ODR_2
+	
 /* Private variables ---------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
@@ -48,47 +52,52 @@ void SysTick_Handler()
   */
 int main(void)
 {
-    GPIO_InitTypeDef initStruct;
- 
-    /* init the sys tick counter (needed for SysDelay) */
-    SysTick_Init();
-    
-    /* enable clock for GPIO C */
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+	SysTick_Init();
+	/*
+	
+		GPIO_InitTypeDef initStruct;
+	
+	GPIO_StructInit(&initStruct);
+	initStruct.GPIO_Pin = GPIO_Pin_0;
+	initStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_Init(GPIOC, &initStruct);
+	
+	
+	*/
 
-    /* Configure GPIO pin for LED3 as output (connected to PC0) */
-    GPIO_StructInit(&initStruct);
-    initStruct.GPIO_Pin = GPIO_Pin_0;
-    initStruct.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_Init(GPIOC, &initStruct);
-
-    while (1)
-    {
-        /* disable LED3 (set pin to high because of negative logic) */
-        GPIO_SetBits(GPIOC, GPIO_Pin_0);
-        SysDelay_Delay(200);
-        /* enable LED3 */
-        GPIO_ResetBits(GPIOC, GPIO_Pin_0);
-        SysDelay_Delay(200);
-    }
-
-    /* alternative using direct register access */
-
-    /*
+	
+	
+	
     // enable clock for GPIO C
     RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
 
     // set pins 0-2 on port C to output
-    GPIOC->MODER = (1 << (2*0));
-
-    while (1)
+		GPIOC->MODER |=  GPIO_MODER_MODER0_0 | GPIO_MODER_MODER1_0 | GPIO_MODER_MODER2_0;
+	
+		char Led_Mask = 0x01;
+		GPIOC->ODR = (GPIO_ODR_0 & ~0x07)| ~Led_Mask;
+	
+while (1)
     {
-        // disable LED3 (set pin to high because of negative logic)
-        GPIOC->ODR = GPIO_Pin_0;
+				Led_Mask = (Led_Mask << 1);
+        GPIOC->ODR = (GPIOC->ODR & ~0x07)| ~Led_Mask;
+				
         SysDelay_Delay(200);
-        // enable LED3
-        GPIOC->ODR = 0;
+			
+				Led_Mask = (Led_Mask << 1);
+				GPIOC->ODR = (GPIOC->ODR & ~0x07)| ~Led_Mask;
+				
         SysDelay_Delay(200);
+			
+				Led_Mask = (Led_Mask >> 1);
+				GPIOC->ODR = (GPIOC->ODR & ~0x07)| ~Led_Mask;
+			
+				SysDelay_Delay(200);
+			
+				Led_Mask = (Led_Mask >> 1);
+				GPIOC->ODR = (GPIOC->ODR & ~0x07)| ~Led_Mask;
+			
+				SysDelay_Delay(200);
     }
-    */
+    
 }
