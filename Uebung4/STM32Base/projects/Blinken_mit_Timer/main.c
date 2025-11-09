0ded7c67cb3ac807ad7c0482c8ebdb754a28f030
flashfishgit <102473074+flashfishgit@users.noreply.github.com>
2025-11-09 23:22:17 +0100

    Programm Ue4

--- Uebung4/STM32Base/projects/Blinken_mit_Timer/main.c
+++ Uebung4/STM32Base/projects/Blinken_mit_Timer/main.c
@@ -21,12 +21,24 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
+// 2Hz  1:5
+#define BTN_USER0_PSC (1000-1)
+#define BTN_USER0_ARR (24000-1)
+#define BTN_USER0_CCR (4000)
+
+// 4Hz 1:3
+#define BTN_USER1_PSC (1000-1)
+#define BTN_USER1_ARR (12000-1)
+#define BTN_USER1_CCR (3000)
+
+// 9 Hz 3:2
+#define BTN_WAKEUP_PSC (1000-1)
+#define BTN_WAKEUP_ARR (5333-1)
+#define BTN_WAKEUP_CCR (3200)
 
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 static volatile uint32_t g_ms = 0;
-static bool zone1_active = false;
-static bool zone2_active = false;
 
 /* Private function prototypes -----------------------------------------------*/
 static void SysTick_Init(void);
@@ -45,6 +57,7 @@ static void SysTick_Init(void) {
   SysTick_Config(SystemCoreClock / 1000);
 }
 
+
 /**
   * @brief  This is the SysTick interrupt handler which is called every 1ms.
             We have to increment the HAL tick counter which is used for SysDelay
@@ -57,34 +70,43 @@ void SysTick_Handler() {
 }
 
 
-void TIM3_InitForCompareOutput(){
+void TIM3_InitForCompareOutput(uint16_t psc, uint16_t arr, uint16_t ccr3){
+	// Activate GPIO clock when not activated
+	if (!(RCC->AHBENR & RCC_AHBENR_GPIOCEN)) {
+     RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+  }
+	// Activate timer 3 clock
+	 if (!(RCC->APB1ENR & RCC_APB1ENR_TIM3EN)) {
+     RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+  }
+	 
+	// Stop timer if already running
+	TIM3->CR1 &= ~TIM_CR1_CEN;
+	// Disable before configruation
+	TIM3->CCER &= ~TIM_CCER_CC3E;
+	
+	// Initalise Pin
 	// configure PC8 to AF mode
 	GPIO_InitTypeDef GPIO_initStruct;
-	
-	// Activate GPIO clock
-	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
-	
 	GPIO_StructInit(&GPIO_initStruct);
 	GPIO_initStruct.GPIO_Pin = GPIO_Pin_8;
-	GPIO_initStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_initStruct.GPIO_Mode = GPIO_Mode_AF; // configur alternate function
 	GPIO_Init(GPIOC, &GPIO_initStruct);
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource8, GPIO_AF_0); 
 	
-	//configure timer 3 to toggle PC8 with 1 Hz
-	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+		// configure output compare mode to PWM mode 1
+	TIM3->CCMR2 = (TIM3->CCMR2 & ~(7u<<4))|(6u << 4);
+	//TIM3->CR1   |= TIM_CR1_ARPE; 
 	
-	// 1000L = Prescaler=1000, 2 = 50% high/50% low
-	TIM3->ARR = (SystemCoreClock / 1000L ) -1 ;
-	TIM3->PSC = (1000 -1);
+	// Sets the timer freq and DutyCylce
+	TIM3->ARR = arr;
+	TIM3->PSC = psc;
+	TIM3->CCR3 = ccr3;
 	
-	TIM3->CCR3 = (SystemCoreClock / 1000L / 2UL) -1;
-	
-	// configure output compare mode to PWM mode 2
-	TIM3->CCMR2 |= (7 << 4);
+                               
 	
 	// enable output control of pin
 	TIM3->CCER |= TIM_CCER_CC3E;
-	
 	//start the compare match timer
 	TIM3->CR1 |= TIM_CR1_CEN;
 }
@@ -93,15 +115,19 @@ int main(void) {
   SysTick_Init();
   LED_Initialize();
   Button_Initialize();
-
-  uint32_t lastToggle = 0;
-  bool switchedHSI = false;
-
-	TIM3_InitForCompareOutput();
 	
+	// Falls mehrere Knöpfe gedrückt werden gewinnte das erste in der if
   while (1) {
-   
+   uint32_t button = Button_GetState();
+		
+		if(button & BUTTON_USER0_MASK){
+			TIM3_InitForCompareOutput(BTN_USER0_PSC, BTN_USER0_ARR, BTN_USER0_CCR);
+		} else if(button & BUTTON_USER1_MASK){
+			TIM3_InitForCompareOutput(BTN_USER1_PSC, BTN_USER1_ARR, BTN_USER1_CCR);
+		} else if(button & BUTTON_WAKEUP_MASK){
+			TIM3_InitForCompareOutput(BTN_WAKEUP_PSC, BTN_WAKEUP_ARR, BTN_WAKEUP_CCR);
+		}
 
-    SysDelay_Delay(1);
+    SysDelay_Delay(10);
   }
 }
